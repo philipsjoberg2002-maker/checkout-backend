a22f32f0fcf48ba4702d508e3e384720014325f1
philipsjoberg2002-maker <philipsjoberg2002@gmail.com>
2026-04-09 12:14:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post("/create-checkout", async (req, res) => {
     }
 
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
+      payment_method_types: ["card", "klarna"],
 
       mode: "payment",
 
